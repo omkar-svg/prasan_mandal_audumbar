53e4534f1abbe979692b7f9262828e04158c7ae2
omkar-svg <omkargaikwadoneplus@gmail.com>
2026-08-24 15:17:38 +0530

    ready for deploy

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -9,6 +9,7 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
+    dialectModule: require('pg'),
     logging: false,
 
     dialectOptions: {
